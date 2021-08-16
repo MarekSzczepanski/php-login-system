d61e23b0ae52c1af92ebcd2d0d2ea2265d511bcd
Marek <marek@ilieth.at>
2021-08-17 00:59:48 +0200

    Add register system

--- register.php
+++ register.php
@@ -13,6 +13,22 @@ elseif (empty($_POST['password'])) {
 }
 else {
     require_once('db.php');
+
+    $username = mysqli_real_escape_string($conn, $_REQUEST['name']);
+    $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
+    $password = mysqli_real_escape_string($conn, $_REQUEST['password']);
+
+    $sql = "INSERT INTO users (username, email, pass) VALUES (?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        echo "Statement prepare failed";
+    }
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashed_password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    echo $username;
 }
 
 require_once('footer.php');
