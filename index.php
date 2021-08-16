d5d9e5c26233939b8c37f00830deeb7c9d642a72
Marek <marek@ilieth.at>
2021-08-16 23:28:13 +0200

    Establish database connection

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Login System</title>
-</head>
-<body>
-    <form method="post" action="/register.php" name="register">
+<?php require_once('header.php'); ?>
+    <form method="post" action="/php-login-system/register.php">
         <input type="text" name="name" placeholder="Your name" />
         <input type="email" name="email" placeholder="Your e-mail" />
         <input type="password" name="password" placeholder="Your password" />
-        <input type="submit" name="submit" value="Submit"/>
+        <input type="submit" name="register" value="Submit"/>
     </form>
-    <?php
-        echo "echo test";
-    ?>
-</body>
-</html>
+<?php
+    require_once('footer.php');
+?>
--- register.php
+++ register.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "register test";
+require_once('header.php');
+
+if (empty($_POST['name'])) {
+    echo "Choose your name";
+}
+elseif (empty($_POST['email'])) {
+    echo "Choose your e-mail";
+}
+elseif (empty($_POST['password'])) {
+    echo "Choose your password";
+}
+else {
+    require_once('db.php');
+}
+
+require_once('footer.php');
